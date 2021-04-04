ac7a289ff04c992791dd563a0b36b2d5603dcc23
Niklaas Cotta <nikcotta@gmail.com>
2021-04-04 15:31:24 -0700

    Test

--- Scraper.py
+++ Scraper.py
@@ -3,4 +3,13 @@ Web Scraper for Reddit
 Author: Niklaas Cotta
 """
 
-print("Test")
+from bs4 import BeautifulSoup
+
+
+with open('test.html', 'r') as html_file:
+    content = html_file.read()
+
+    soup = BeautifulSoup(content, 'lxml')
+    # print(soup.prettify())
+    tags = soup.find('span')
+    print(tags)
