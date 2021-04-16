7428ca6db31862c660124511775986c54b6437c4
Niklaas Cotta <nikcotta@gmail.com>
2021-04-16 15:51:56 -0700

    Finally got it working, need a specific header that for some reason works when others don't. Amazon uses JavaScript to grab third party pricing so I will do that soon

--- Scraper.py
+++ Scraper.py
@@ -1,35 +1,46 @@
 """
-Web Scraper for Reddit
+Web Scraper for Amazon
 Author: Niklaas Cotta
+Used this video as a tutorial: https://www.youtube.com/watch?v=Bg9r_yLk7VY
 """
 
 from bs4 import BeautifulSoup
 import requests
-# 502 error, need an API for reddit?
-
-url = 'https://www.reddit.com/r/whatsthisplant/'
-status = requests.get(url)
-print(status)  # Status code 200 = good!
-
-html_text = requests.get('https://www.reddit.com/r/whatsthisplant/').text
-soup = BeautifulSoup(html_text, 'lxml')  # parser
-
-posts = soup.find_all('div', class_='rpBJOHq2PR60pnwJlUyP0')
-print(posts)
 
 
-"""""
-with open('test.html', 'r') as html_file:
-    content = html_file.read()
+def pricing():
+    url = 'https://www.amazon.com/dp/B08G5CQMJ3/ref=olp_aod_early_redir?_encoding=UTF8&aod=1&qid=1618611274&sr=8-3'
 
-    soup = BeautifulSoup(content, 'lxml')
-    # print(soup.prettify())
-    tags = soup.find_all('tr')
-    for tag in tags:
-        tag_td = tag.td
-        if tag.a is not None:
-            tag_a = tag.a.split()[-1]
-            print(tag_a)
+    # Doesn't work otherwise!!
+    headers = {"User-Agent": 'Mozilla/5.0 (X11; Linux x86_64)', 'Cache-Control': 'no-cache', "Pragma": "no-cache"}
 
-        print(tag_td)
-"""""
+    page = requests.get(url, headers=headers)
+
+    soup = BeautifulSoup(page.content, 'lxml')
+
+    print(soup.prettify())
+
+    productName = soup.find(id="productTitle").get_text()
+
+    try:
+        price = soup.find(id="priceblock_ourprice").get_text()
+    except AttributeError:
+        price = ""
+
+    return productName, price
+
+
+def printPrice(product):
+    (name, price) = product
+    print("Name: ", name.strip()[0:64])
+    print("Price: ", price.strip())
+
+    if not price:
+        # thirdParty =
+        print("Product is not available for purchase by amazon at this time")
+        print("Cheapest third party price: ")
+
+
+if __name__ == '__main__':
+    productPrice = pricing()
+    printPrice(productPrice)
